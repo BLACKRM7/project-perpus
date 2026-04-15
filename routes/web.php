ea7ee65d27a1a2c5b4ec105f38de86f260c77083
BLACKRM7 <moch.romi.syahputra@gmail.com>
2026-04-15 18:00:49 +0700

    add pages

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,28 @@ Route::get('/', function () {
     return view('pages.beranda.index');
 })->name('beranda');
 
+Route::get('/buku', function () {
+    return view('pages.buku.index');
+})->name('buku');
+
 Route::get('/login', function () {
     return view('pages.login');
 })->name('login');
+
+Route::get('/dashboard', function () {
+    return view('pages.dashboard.index');
+})->name('dashboard');
+
+Route::get('/anggota', function () {
+    return view('pages.anggota.index');
+})->name('anggota');
+
+Route::get('/peminjaman', function () {
+    return view('pages.peminjaman.index');
+})->name('peminjaman');
+
+Route::get('/petugas', function () {
+    return view('pages.petugas.index');
+})->name('petugas');
+
+?>
